2ca03fb556bc8107f4a96765eb86ec8b10a935ab
Rex <vivelarepublique@qq.com>
2022-08-14 00:25:30 +0800

    2nd release 0814

--- CDRB.user.js
+++ CDRB.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CDRB
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Code Doesn't Require BAIDU.
 // @author       vivelarepublique
 // @match        https://www.baidu.com/*
@@ -12,7 +12,14 @@
 (function () {
     'use strict';
 
-    // Your code here...
+    const otherResult = [
+        'CSDN',
+        //这里可以添加其余想屏蔽的关键词
+        //添加格式为 ==》 " '想屏蔽的关键词', "
+        //！！！注意：不含""双引号，但是包含''单引号，而且,英文逗号不能省略！！！
+        //↓请将光标放在此段末尾（最后一个向下箭头的后面），然后按下回车，在此行下面一行进行添加↓
+        '脚本之家',
+    ];
 
     const targetNode = document.getElementById('wrapper');
 
@@ -38,12 +45,12 @@
         let ADlist2 = document.getElementsByClassName('c-gap-left');
 
         if (ADlist.length) {
-            console.log(ADlist);
             for (let ad of ADlist) {
                 let node = ad;
-                while (node.id !== 'content_left') {
+                while (node && node.id !== 'content_left') {
                     if (node) {
-                        if (node.className.includes('c-container')) {
+                        if (node.className?.includes('c-container')) {
+                            console.log('移除了一个广告：', ad.parentNode?.parentNode?.innerText);
                             node.remove();
                             break;
                         } else {
@@ -56,16 +63,64 @@
 
         if (ADlist2.length) {
             for (let ad of ADlist2) {
-                console.log(ADlist2);
                 let node = ad;
-                while (node.id !== 'content_left') {
+                while (node && node.id !== 'content_left') {
                     if (node) {
-                        if (node.className.includes('c-container')) {
+                        if (node.className?.includes('c-container')) {
+                            console.log('移除了一个广告：', ad.parentNode?.parentNode?.innerText);
                             node.remove();
                             break;
                         } else {
                             node = node.parentNode;
                         }
+                    }
+                }
+            }
+        }
+
+        let linkList = document.getElementsByTagName('a');
+        let spanList = document.getElementsByClassName('c-color-gray item-site-name_3aKAy');
+        let searchingValue = document.getElementById('kw')?.value;
+
+        if (linkList.length) {
+            for (let link of linkList) {
+                let node = link;
+                for (let other of otherResult) {
+                    if (link.innerHTML.includes(other) && !searchingValue?.includes(other)) {
+                        while (node && node.id !== 'content_left') {
+                            if (node) {
+                                if (node.className?.includes('c-container')) {
+                                    console.log('移除了一个关键词：', other);
+                                    node.remove();
+                                    break;
+                                } else {
+                                    node = node.parentNode;
+                                }
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (spanList.length) {
+            for (let span of spanList) {
+                let node = span;
+                for (let other of otherResult) {
+                    if (span.innerHTML.includes(other) && !searchingValue?.includes(other)) {
+                        while (node && node.id !== 't c-line-clamp1') {
+                            if (node) {
+                                if (node.className?.includes('c-gap-bottom-small')) {
+                                    console.log('移除了一个关键词：', other);
+                                    node.remove();
+                                    break;
+                                } else {
+                                    node = node.parentNode;
+                                }
+                            }
+                        }
+                        break;
                     }
                 }
             }
